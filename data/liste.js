74ea9c373db0964918ac781c02a98c0244ee8796
Marie-Sophie <mayormaries@gmail.com>
2017-10-23 11:58:40 +0200

    liste (nf)

---
+++ data/liste.js
@@ -0,0 +1,16 @@
+const srcListe = [
+ {
+   "nom": "MAYOR",
+   "prenom": "Marie-Sophie",
+   "contact_mail": "mayormaries@gmail.com"
+ },
+
+ {
+   "nom": "WIDLOECHER",
+   "prenom": "Amélie",
+   "contact_mail": "maildamelie@gmail.com"
+ }
+];
+
+module.exports = srcListe;
+
